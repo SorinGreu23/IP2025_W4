4caf50145e9d8e570d1a5d194fa43fa2e5f23fd5
AlexBaam <alexacasandrei0307@gmail.com>
2025-03-18 09:14:00 +0200

    Main file

--- IP_Lab4/src/main/java/org/example/Main.java
+++ IP_Lab4/src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        BankEmployee employee = new BankEmployee("Nicu", "Acasandrei", 21, "AleAca@gmail.com", "EMP123");
+
+        AI ai = new AI(employee);
+
+        BankCustomer customer = new BankCustomer("Sorin", "Greu", 21, "SorinGreu2003@gmail.com", "CUST456", ai);
+
+        employee.setCustomer(customer);
+
+        customer.askQuesstion("Cum pot obține un împrumut pentru casă?");
     }
 }
